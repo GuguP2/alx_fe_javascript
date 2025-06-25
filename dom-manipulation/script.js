7d6412295e3f2ca8f134445de444aa12c1366ec6
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-25 20:40:02 +0200

    Update script.js

---
+++ dom-manipulation/script.js
@@ -0,0 +1,11 @@
+const quoteGen = [{text: 'you are brave', category:'self love'},
+  {text: 'gym is life', category:'self talk'},
+  {text: 'you are beautiful', category:'self love'}
+];
+
+function randomQuote(){
+  const selector = Math.floor(Math.random() * quoteGen.length);
+  const selectedQuote = quoteGen[selector];
+  const displayQuote = document.getElementById('quoteDisplay');
+  displayQuote.innerHTML = `<p>${quoteGen.text}</p><small>${quoteGen.category}</small>`;
+}
