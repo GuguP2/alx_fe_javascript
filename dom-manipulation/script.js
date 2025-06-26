93436df541c35738c1cc1a1d189c9b32613d0efe
Gugu Mthethwa <pociamthethwa@gmail.com>
2025-06-26 19:02:54 +0200

    json

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -64,3 +64,13 @@ window.onload = function () {
     refreshBtn.addEventListener("click", showRandomQuote);
   }
 };
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
